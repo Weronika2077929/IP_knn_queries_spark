1f5603f9bfe4e42be122433b1a6b96858807e9b7
JeszuaHaNocri <lachtaraweronika@gmail.com>
2016-12-09 17:52:46 +0000

    Finalised Spark Core solution for finding the k nearest points

--- src/main/java/KnnQueriesSparkCore.java
+++ src/main/java/KnnQueriesSparkCore.java
@@ -10,6 +10,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.DoubleFlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import scala.Tuple2;
+import scala.Tuple3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,34 +33,24 @@ public class KnnQueriesSparkCore {
         SparkConf conf = new SparkConf().setAppName("knn_queries_spark_core").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> distFile = sc.textFile("C:/Users/Wera/Documents/4thyear/IP/Java_Spark_Project/src/main/resources/10.csv");
+        JavaRDD<String> pointsfromFileString = sc.textFile("C:/Users/Wera/Documents/4thyear/IP/Java_Spark_Project/src/main/resources/10.csv");
 
-
-//        JavaRDD<Double[]> map = distFile.map(s -> {
-//            String[] arr = s.split(",");
-//            Double[] doubleArr = new Double[arr.length];
-//            for(int i = 0; i < arr.length; i++) {
-//                doubleArr[i] = Double.parseDouble(arr[i]);
-//            }
-//            return doubleArr;
-//        });
-
-
-        JavaRDD<Tuple2<Double,Double>> map = distFile.map(s -> {
+        JavaRDD<Tuple2<Double,Double>> pointsfromFileTuple = pointsfromFileString.map(s -> {
             String[] arr = s.split(",");
             return Tuple2.apply(Double.parseDouble(arr[0]), Double.parseDouble(arr[1]));
         });
 
-        for( Tuple2 s : map.collect()){
-            System.out.println(s._1 + " " + s._2);
-        }
+        JavaRDD<Tuple3<Double,Double,Double>> distancesToPoints = pointsfromFileTuple.map(s -> {
+            double distance = Math.sqrt(Math.pow(x_coordinate - s._1, 2) + Math.pow(y_coordinate - s._2, 2));
 
-        JavaRDD<Double> map2 = map.map(s -> Math.sqrt(Math.pow(x_coordinate - s._1, 2) + Math.pow(y_coordinate - s._2 , 2)));
+            return new Tuple3<Double, Double, Double>(s._1, s._2, distance);
+        });
+
+        distancesToPoints = distancesToPoints.sortBy(x -> x._3(), true, 1);
 
         System.out.println();
-        for( Double s : map2.collect()){
+        for (Tuple3 s : distancesToPoints.take(k)){
             System.out.println(s);
         }
-
     }
 }
