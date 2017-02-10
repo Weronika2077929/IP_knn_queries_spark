9a6eb7415a0a1e82dcdb1bafbf693ec062c268f1
JeszuaHaNocri <lachtaraweronika@gmail.com>
2017-02-10 19:24:28 +0000

    Fixed SparkSQL implementation for kNN Queries

--- src/main/java/KnnQueriesSparkSQL.java
+++ src/main/java/KnnQueriesSparkSQL.java
@@ -12,13 +12,22 @@ import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.catalog.Function;
 import org.apache.spark.sql.types.DataTypes;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
 import java.util.function.BiFunction;
 
 import static spark.Spark.*;
 
 public class KnnQueriesSparkSQL {
-    public static void main(String[] args) {
+    private static String FILE_PATH = "C:/Users/Wera/Documents/4thyear/IP/Java_Spark_Project/src/main/resources/";
+    private static String FILE_NAME_DATASET = FILE_PATH + "1000";
+    private static String FILE_NAME_QUERY_POINTS = FILE_PATH + "10";
 
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         get("/hello", (req, res) -> "Hello world");
 
 //        number of nn queries to find
@@ -27,6 +36,19 @@ public class KnnQueriesSparkSQL {
 //        the coordinates of the main point of reference
         double x_coordinate = 0;
         double y_coordinate = 0;
+
+
+        LinkedList<Double[]> queryPoints = new LinkedList<>();
+        try {
+            Scanner in = new Scanner(new FileReader(FILE_NAME_QUERY_POINTS));
+            while(in.hasNext()) {
+                String[] data = in.nextLine().split(",");
+                Double[] a = {Double.parseDouble(data[0]),Double.parseDouble(data[1])};
+                queryPoints.add(a);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
 //        disable the log messages
         Logger.getLogger("org").setLevel(Level.OFF);
@@ -38,28 +60,26 @@ public class KnnQueriesSparkSQL {
                 .config("spark.some.config.option", "some-value")
                 .getOrCreate();
 
-        Dataset<Row> df = spark.read().csv("C:/Users/Wera/Documents/4thyear/IP/Java_Spark_Project/src/main/resources/10000.csv");
+        Dataset<Row> df = spark.read().csv(FILE_NAME_DATASET);
 
-//        System.out.println("min " + df.filter(min("_c0")).show(); );
-//        df.groupBy("_c0").show();
+        df.createOrReplaceTempView("points");
 
-//        Column c0 = df.col("_c0");
-//
-//        for(Row row: df.collectAsList()) {
-//            Double x = Double.parseDouble(row.getString(0));
-//            Double y = Double.parseDouble(row.getString(1));
-//            System.out.println("x: " + x + ", y: " +y);
-//        }
+//        Dataset<Row> sqlVarriable = spark.sql("SELECT * FROM points ORDER BY POWER( " + x_coordinate + " + _c0 , 2) + POWER(" + y_coordinate + " + _c1 , 2) LIMIT " + k);
+//        sqlVarriable.show();
 
-        SQLContext sqlContext = new SQLContext(spark);
+        for( Double[] queryPoint : queryPoints){
+            System.out.println(queryPoint[0] + " " + queryPoint[1]);
+             Dataset<Row> sqlVar = spark.sql("SELECT * FROM points ORDER BY SQRT(POWER( "
+                     + queryPoint[0]
+                     + " - _c0 , 2) + POWER("
+                     + queryPoint[1]
+                     + " - _c1 , 2))");
 
-        sqlContext.udf().register("myFunction", (String x, String y) ->
-                    Math.sqrt(Math.pow(x_coordinate - Double.parseDouble(x), 2) + Math.pow(y_coordinate - Double.parseDouble(y), 2)
-                ), DataTypes.DoubleType);
+            sqlVar.show(k);
+            break;
+        }
 
-        Column distance = callUDF("myFunction", col("_c0"), col("_c1"));
-        df = df.withColumn("distance", distance).orderBy("distance");
-
-        df.show(k);
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println(estimatedTime + " miliseconds");
     }
 }
